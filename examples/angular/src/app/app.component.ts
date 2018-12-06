cb5cf7f01a1400f1a46ba94af79da499dbe0b96e
Marei Kikukawa <contact@calmery.me>
2018-12-06 16:08:37 +0900

    Angular のサンプルコードを追加する

---
+++ examples/angular/src/app/app.component.ts
@@ -0,0 +1,41 @@
+import { Component, ViewChild, AfterViewInit } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent implements AfterViewInit {
+  @ViewChild('elm') ref;
+
+  title = 'angular';
+
+  public userId = 'calmery';
+  public value = '';
+  public disabled = false;
+
+  ngAfterViewInit() {
+    const elm = this.ref.nativeElement;
+
+    elm.addEventListener('load', this._onLoad.bind(this));
+    elm.addEventListener('error', this._onError.bind(this));
+  }
+
+  public onChange(event) {
+    this.value = event.target.value;
+  }
+
+  public onClick() {
+    this.userId = this.value;
+    this.value = '';
+    this.disabled = true;
+  }
+
+  private _onLoad() {
+    this.disabled = false;
+  }
+
+  private _onError() {
+    this.disabled = false;
+  }
+}
